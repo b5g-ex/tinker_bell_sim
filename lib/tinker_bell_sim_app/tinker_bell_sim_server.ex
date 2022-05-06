88108a758f872c55325d2d2093845222d9cbcca0
{DaisukeSasaki} <{sasaki-daisuke665@g.ecc.u-tokyo.ac.jp}>
2022-05-06 15:07:45 +0900

    map revised

--- lib/tinker_bell_sim_app/tinker_bell_sim_server.ex
+++ lib/tinker_bell_sim_app/tinker_bell_sim_server.ex
@@ -8,16 +8,20 @@ defmodule TinkerBellSimServer do
 
   def handle_call(:startworkterm, _from, state) do
     for times <- 0..4 do
-      workerstate = GenServer.call(Enum.at(Enum.at(state, times),0), :sendstate)
-      #IO.inspect workerstate
-      #GenServer.call(Server, {:append_workerstate, workerstate, times})
-      #Enum.at(Enum.at(state, 0), times) = workerstate
+      workerstate = GenServer.call(Enum.at(Map.keys(state),times), :sendstate)
+      Map.get_and_update(state, Enum.at(Map.keys(state),times), fn current_state
+        -> {current_state, workerstate}
+      end)
     end
     {:reply, state, state}
   end
 
   def handle_call({:append_workerinfo, pid, calcpower}, _from, state) do
-    Map.put(state, pid, calcpower)
+    state = Map.put_new(state, pid, calcpower)
+    {:reply, state, state}
+  end
+
+  def handle_call(:getstate, _from, state) do
     {:reply, state, state}
   end
 
@@ -28,6 +32,9 @@ defmodule TinkerBellSimServer do
       {:ok, pid} = TinkerBellSimWorker.start_link(100 * (times+1))
       GenServer.call(Server, {:append_workerinfo, pid, 100 * (times+1)})
     end
+
+    GenServer.call(Server,:getstate)
+
   end
 
   def startworkterm do
